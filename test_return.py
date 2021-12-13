81e0b2e7d56d269ccb5e524b6527e5b9e38bb286
Kalsani Ram hruthik reddy <51695155+ramhruthik@users.noreply.github.com>
2021-12-12 20:58:25 -0600

    Create test_return.py

---
+++ test_return.py
@@ -0,0 +1,19 @@
+import time
+from robot_control_class import RobotControl #importa uma classe
+robotcontrol = RobotControl() #criando um objeto
+
+def robotmove(a):
+    robotcontrol.move_straight()
+    time.sleep(a)
+    robotcontrol.stop_robot()
+
+print('Provide 3 values between 0 and 719.')
+value = [0,1,2]
+value[0] = input('value 1:')
+value[1] = input('value 2:')
+value[2] = input('value 3:')
+print(value)
+
+#robotmove(5)
+laser1 = robotcontrol.get_laser(360)
+print(laser1)
