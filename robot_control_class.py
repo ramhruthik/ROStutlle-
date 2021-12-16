9b1b948d6cf759e6b9f8d863848c8da1332fdb5b
Kalsani Ram hruthik reddy <51695155+ramhruthik@users.noreply.github.com>
2021-12-16 10:16:29 -0600

    Update robot_control_class.py

--- robot_control_class.py
+++ robot_control_class.py
@@ -1,136 +1,136 @@
-import rospy
 from geometry_msgs.msg import Twist
-from sensor_msgs.msg import LaserScan
+import rospy as rp
+
 import time
+from sensor_msgs.msg import LaserScan 
 
 
 class RobotControl():
 
     def __init__(self):
-        rospy.init_node('robot_control_node', anonymous=True)
-        self.vel_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
-        self.laser_subscriber = rospy.Subscriber(
+        rp.init_node('robot_control_node', anonymous=True)
+        self.vel_publisher_v = rp.Publisher('/cmd_vel', Twist, queue_size=1)
+        self.laser_subscriber_v = rp.Subscriber(
             '/kobuki/laser/scan', LaserScan, self.laser_callback)
-        self.cmd = Twist()
-        self.laser_msg = LaserScan()
-        self.ctrl_c = False
-        self.rate = rospy.Rate(1)
-        rospy.on_shutdown(self.shutdownhook)
+        self.commands = Twist()
+        self.laser_msgs = LaserScan()
+        self.ctrl_c_f = False
+        self.rate_val = rp.Rate(1)
+        rp.on_shutdown(self.shutdownhook)
 
     def publish_once_in_cmd_vel(self):
-        while not self.ctrl_c:
-            connections = self.vel_publisher.get_num_connections()
-            if connections > 0:
-                self.vel_publisher.publish(self.cmd)
-                #rospy.loginfo("Cmd Published")
+        while not self.ctrl_c_f :
+            connections_num = self.vel_publisher_v.get_num_connections()
+            if connections_num > 0:
+                self.vel_publisher_v.publish(self.commands)
+
                 break
             else:
-                self.rate.sleep()
+                self.rate_val.sleep()
 
     def shutdownhook(self):
         # works better than the rospy.is_shutdown()
-        self.ctrl_c = True
+        self.ctrl_c_f  = True
 
     def laser_callback(self, msg):
-        self.laser_msg = msg
+        self.laser_msgs = msg
 
     def get_laser(self, pos):
         time.sleep(1)
-        return self.laser_msg.ranges[pos]
+        return self.laser_msgs.ranges[pos]
 
     def get_front_laser(self):
         time.sleep(1)
-        return self.laser_msg.ranges[360]
+        return self.laser_msgs.ranges[360]
 
     def get_laser_full(self):
         time.sleep(1)
-        return self.laser_msg.ranges
+        return self.laser_msgs.ranges
 
     def stop_robot(self):
-        #rospy.loginfo("shutdown time! Stop the robot")
-        self.cmd.linear.x = 0.0
-        self.cmd.angular.z = 0.0
+
+        self.commands.linear.x = 0.0
+        self.commands.angular.z = 0.0
         self.publish_once_in_cmd_vel()
 
     def move_straight(self):
 
         # Initilize velocities
-        self.cmd.linear.x = 0.5
-        self.cmd.linear.y = 0
-        self.cmd.linear.z = 0
-        self.cmd.angular.x = 0
-        self.cmd.angular.y = 0
-        self.cmd.angular.z = 0
+        self.commands.linear.x = 0.5
+        self.commands.linear.y = 0
+        self.commands.linear.z = 0
+        self.commands.angular.x = 0
+        self.commands.angular.y = 0
+        self.commands.angular.z = 0
 
         # Publish the velocity
         self.publish_once_in_cmd_vel()
 
-    def move_straight_time(self, motion, speed, time):
+    def mov_straight_time_val(self, motion, speed, time):
 
-        # Initilize velocities
-        self.cmd.linear.y = 0
-        self.cmd.linear.z = 0
-        self.cmd.angular.x = 0
-        self.cmd.angular.y = 0
-        self.cmd.angular.z = 0
+        self.commands.linear.y = 0
+        self.commands.linear.z = 0
+        self.commands.angular.x = 0
+        self.commands.angular.y = 0
+        self.commands.angular.z = 0
 
         if motion == "forward":
-            self.cmd.linear.x = speed
+            self.commands.linear.x = speed
         elif motion == "backward":
-            self.cmd.linear.x = - speed
+            self.commands.linear.x = - speed
 
-        i = 0
-        # loop to publish the velocity estimate, current_distance = velocity * (t1 - t0)
-        while (i <= time):
+        loop = 0
 
-            # Publish the velocity
-            self.vel_publisher.publish(self.cmd)
-            i += 1
-            self.rate.sleep()
+        while (loop <= time):
 
-        # set velocity to zero to stop the robot
+
+            self.vel_publisher_v.publish(self.commands)
+            loop += 1
+            self.rate_val.sleep()
+        motion1=motion
+
         self.stop_robot()
 
-        s = "Moved robot " + motion + " for " + str(time) + " seconds"
+        s = "Moved robot " + motion1 + " for " + str(time) + "  seconds"
         return s
 
 
     def turn(self, clockwise, speed, time):
 
         # Initilize velocities
-        self.cmd.linear.x = 0
-        self.cmd.linear.y = 0
-        self.cmd.linear.z = 0
-        self.cmd.angular.x = 0
-        self.cmd.angular.y = 0
+        self.commands.linear.x = 0
+        self.commands.linear.y = 0
+        self.commands.linear.z = 0
+        self.commands.angular.x = 0
+        self.commands.angular.y = 0
 
 
         if clockwise == "clockwise":
-            self.cmd.angular.z = -speed
+            self.commands.angular.z = -speed
         else:
-            self.cmd.angular.z = speed
+            self.commands.angular.z = speed
 
-        i = 0
-        # loop to publish the velocity estimate, current_distance = velocity * (t1 - t0)
-        while (i <= time):
+        loop = 0
 
-            # Publish the velocity
-            self.vel_publisher.publish(self.cmd)
-            i += 1
-            self.rate.sleep()
+        while (loop <= time):
 
-        # set velocity to zero to stop the robot
+
+            self.vel_publisher_v.publish(self.commands)
+            loop += 1
+            self.rate_val.sleep()
+
+
         self.stop_robot()
-
-        s = "Turned robot " + clockwise + " for " + str(time) + " seconds"
+        clk=clockwise
+        s = "Turned robot " + clk + " for " + str(time) + "  seconds"
         return s
 
 
 if __name__ == '__main__':
-    #rospy.init_node('robot_control_node', anonymous=True)
-    robotcontrol_object = RobotControl()
-    try:
-        robotcontrol_object.move_straight()
 
-    except rospy.ROSInterruptException:
+    robotcontrol_object_new = RobotControl()
+    try:
+        robotcontrol_object_new.move_straight()
+
+    except rp.ROSInterruptException:
         pass
